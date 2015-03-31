7f47b064f57e3b946ed79b5dc5a5f73764792281
Wendy Gwo <wgwo@hotmail.com>
2015-03-30 19:23:52 -0700

    Add default photos for missing photos for users and goods.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,13 @@ angular.module('starter.controllers', [])
 				.get('http://anteater.offerup.c66.me/users/1.json', {cache:true})
 				   .then(function(response){
 				   	$scope.goods = response.data.goods; 
+				   	for (var i=0; i<$scope.goods.length; i++){
+				   		if ($scope.goods[i].photo=='/photos/original/missing.png'){
+				   			$scope.goods[i].photo='img/black-drill-sihouette.png';
+				   		}
+				   	}
 				   	// console.log($scope.goods);
+				   	// console.log($scope.goods.length);
 				   });
 })
 .controller('GoodDetailCtrl', function($scope, $http, $stateParams) {
@@ -17,32 +23,39 @@ angular.module('starter.controllers', [])
 				.get('http://anteater.offerup.c66.me/goods/' + $stateParams.goodId +'.json', {cache:true})
 				   .then(function(response){
 				   	$scope.good = response.data; 
-				   	 
+				   	if ($scope.good.photo =='/photos/original/missing.png'){
+				   		$scope.good.photo = 'img/black-drill-sihouette.png';
+				   	}
 				   });
 })
 
-.controller('ChatsCtrl', function($scope, Chats, $http) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-  	$http
-				.get('http://anteater.offerup.c66.me/goods.json', {cache:true})
-				   .then(function(response){
-				   	$scope.goods = response.data; 
-				   	console.log($scope.goods);
-				   });
-})
+// .controller('ChatsCtrl', function($scope, Chats, $http) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   }
+//   	$http
+// 				.get('http://anteater.offerup.c66.me/goods.json', {cache:true})
+// 				   .then(function(response){
+// 				   	$scope.goods = response.data; 
+// 				   	console.log($scope.goods);
+// 				   });
+// })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
 
 .controller('FriendsCtrl', function($scope, $http) {
 	$http
 				.get('http://anteater.offerup.c66.me/users/1.json', {cache:true})
 				   .then(function(response){
 				   	$scope.friends = response.data.friends; 
+				   	for (var i=0; i<$scope.friends.length; i++){
+				   		if ($scope.friends[i].photo=='/photos/original/missing.png'){
+				   			$scope.friends[i].photo='img/mysteryperson.jpg';
+				   		}
+				   	}
 				   	// console.log($scope.friends);
 				   });
 })
@@ -52,6 +65,11 @@ angular.module('starter.controllers', [])
 				.get('http://anteater.offerup.c66.me/users/' + $stateParams.friendId +'.json', {cache:true})
 				   .then(function(response){
 				   	$scope.goods = response.data.goods; 
+				   	for (var i=0; i<$scope.goods.length; i++){
+				   		if ($scope.goods[i].photo=='/photos/original/missing.png'){
+				   			$scope.goods[i].photo='img/black-drill-sihouette.png';
+				   		}
+				   	}
 				   });
 })
 ;
